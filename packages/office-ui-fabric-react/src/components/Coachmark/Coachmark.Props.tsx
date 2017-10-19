16a7b0ed225e6d5a95bd7f83cc0237ce3f137d71
John Miller <miljo@microsoft.com>
2017-10-19 02:40:23 -0700

    Added onRenderTeachingBubble method

--- packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.Props.tsx
+++ packages/office-ui-fabric-react/src/components/Coachmark/Coachmark.Props.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import { Coachmark } from './Coachmark';
+import { IRenderFunction } from '../../Utilities';
+import { ITeachingBubbleProps } from '../TeachingBubble/TeachingBubble.Props';
 
 export interface ICoachmark {
 
 }
+export interface ICoachmarkStyles {
 
+}
 export interface ICoachmarkProps extends React.Props<Coachmark> {
   /**
   * All props for your component are to be defined here.
   */
   componentRef?: (component: ICoachmark) => void;
+
+  /**
+   * Teaching Bubble onRender override.
+   */
+  onRenderTeachingBubble: IRenderFunction<ITeachingBubbleProps>;
 }
